711d335b805e6bf0a5153c2c34dcb16c6b3b057b
Maruf Robin <maruf.robin@northsouth.edu>
2022-11-15 02:17:35 +0600

    second page work done

--- lib/screen/workout_deatil_page.dart
+++ lib/screen/workout_deatil_page.dart
@@ -1,9 +1,17 @@
 import 'package:fitnessapp/model/fitnessModel.dart';
 import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
-class WorkoutDetailPage extends StatelessWidget {
+class WorkoutDetailPage extends StatefulWidget {
   WorkoutDetailPage({Key? key, required this.fitnessModel}) : super(key: key);
   final FitnessModel fitnessModel;
+
+  @override
+  State<WorkoutDetailPage> createState() => _WorkoutDetailPageState();
+}
+
+class _WorkoutDetailPageState extends State<WorkoutDetailPage> {
+  int second = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +27,50 @@ class WorkoutDetailPage extends StatelessWidget {
               color: Colors.black,
             )),
       ),
-      body: Container(
-          height: MediaQuery.of(context).size.height * 0.6,
-          child: Image.network(
-            "${fitnessModel.thumbnail}",
-            fit: BoxFit.cover,
-          )),
+      body: Column(
+        children: [
+          Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: Image.network(
+                "${widget.fitnessModel.thumbnail}",
+                fit: BoxFit.cover,
+              )),
+          Expanded(
+            child: SleekCircularSlider(
+              appearance: CircularSliderAppearance(),
+              min: 3,
+              max: 30,
+              initialValue: second.toDouble(),
+              onChange: (double value) {
+                setState(() {
+                  second = value.toInt();
+                });
+                // callback providing a value while its being changed (with a pan gesture)
+              },
+              innerWidget: (double value) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "$second",
+                      style: TextStyle(fontSize: 18, color: Colors.purple),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("START"),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.pinkAccent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16))),
+                    )
+                  ],
+                );
+                // use your custom widget inside the slider (gets a slider value from the callback)
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
